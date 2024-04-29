2148e3f34bad88bd167aa4340d24947065c2fe4b
IvanRulevskyi <rulevskiy4@gmail.com>
2024-04-29 23:24:37 +0300

    Initial Commit

---
+++ functor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  functor
+//
+//  Created by Иван Рулевский on 29.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
